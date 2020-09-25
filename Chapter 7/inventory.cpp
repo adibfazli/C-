97eb93aa837ed52c458be6eac231885a1fd588b3
Adib Fazli <adib.fazlii@gmail.com>
2020-09-25 12:52:46 -0700

    c++ chapter 7, working on an error

--- Chapter 7/inventory.cpp
+++ Chapter 7/inventory.cpp
@@ -3,7 +3,7 @@
         void Inventory::setItemNumber(int itemNumber){ this -> itemNumber = itemNumber; };
         void Inventory::setQuantity(int quantity){ this -> quantity = quantity; };
         void Inventory::setCost(double cost){ this -> cost = cost; };
-        void Inventory::setTotalCost(){};
+        void Inventory::setTotalCost(){ totalCost = quantity * cost; };
 
         int Inventory::getItemNumber(){ return itemNumber; };
         int Inventory::getQuantity(){ return quantity; };
